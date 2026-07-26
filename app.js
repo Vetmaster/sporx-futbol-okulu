469410071ffeb911eb6c6e59750860eaaf0b54f3
HASAN SARGIN <hasansargin@HASAN-MacBook-Air.local>
2026-07-26 18:23:04 +0300

    Markaya futbol topu ekle

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.07.26.150';
+const APP_VERSION = '2026.07.26.151';
 const ANDROID_APK_URL = 'https://github.com/Vetmaster/sporx-futbol-okulu/releases/download/v1.0.3-beta/SASA-F-v1.0.3-beta.apk';
 const INSTALL_PROMPT_DISMISS_KEY = 'sasa_install_prompt_dismissed_v1';
 const NATIVE_VERSION_STORAGE_KEY = 'sasa_native_version_code';
