0d5f6185ceed91e60930c1406e217a0a201835bf
HASAN SARGIN <hasansargin@HASAN-MacBook-Air.local>
2026-08-24 23:23:39 +0300

    Show maintenance page on custom domain

--- custom-domain.js
+++ custom-domain.js
@@ -1,5 +1,5 @@
 (() => {
-  const showMaintenanceOnCustomDomain = false;
+  const showMaintenanceOnCustomDomain = true;
   const maintenanceHosts = new Set(['sasa-f.com', 'www.sasa-f.com']);
   if (showMaintenanceOnCustomDomain && maintenanceHosts.has(window.location.hostname.toLowerCase())) {
     document.documentElement.classList.add('show-domain-maintenance');
