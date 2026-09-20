d0b3b01aed093f42a21d30bf398675edec8ad6b1
HASAN SARGIN <hasansargin@HASAN-MacBook-Air.local>
2026-09-20 22:05:39 +0300

    Publish Android v1.0.30 update metadata

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const APP_VERSION = '2026.09.20.424';
-const ANDROID_APK_URL = 'https://github.com/Vetmaster/sporx-futbol-okulu/releases/download/v1.0.29-beta/SASA-F-v1.0.29-beta.apk';
+const ANDROID_APK_URL = 'https://github.com/Vetmaster/sporx-futbol-okulu/releases/download/v1.0.30-beta/SASA-F-v1.0.30-beta.apk';
 const INSTALL_PROMPT_DISMISS_KEY = 'sasa_install_prompt_dismissed_v2';
 const INSTALL_PROMPT_SESSION_DISMISS_KEY = 'sasa_install_prompt_dismissed_this_session';
 const NATIVE_VERSION_STORAGE_KEY = 'sasa_native_version_code';
