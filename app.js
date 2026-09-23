361506d509e792990cf4e5f580c5deb394acaf06
HASAN SARGIN <hasansargin@HASAN-MacBook-Air.local>
2026-09-23 22:05:31 +0300

    Align subscription toolbar controls

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.09.23.475';
+const APP_VERSION = '2026.09.23.476';
 const ANDROID_APK_URL = 'https://github.com/Vetmaster/sporx-futbol-okulu/releases/download/v1.0.30-beta/SASA-F-v1.0.30-beta.apk';
 const INSTALL_PROMPT_DISMISS_KEY = 'sasa_install_prompt_dismissed_v2';
 const INSTALL_PROMPT_SESSION_DISMISS_KEY = 'sasa_install_prompt_dismissed_this_session';
